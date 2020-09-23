7ae50458b7ca167293ee252d1d7a073aa8ceca79
PhilipVigus <philipvigus@yahoo.co.uk>
2020-09-23 15:13:55 +0100

    Connect front and back end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [testData, setTestData] = useState();
+
+  useEffect(() => {
+    const { CancelToken } = axios;
+    const source = CancelToken.source();
+
+    const getData = async () => {
+      try {
+        const body = {
+          query: `
+          query {
+            users {
+              id
+              name
+              email
+            }
+          }
+          `,
+          variables: {},
+        };
+
+        const options = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const result = await axios.post(
+          "http://localhost:4000/graphql",
+          body,
+          options
+        );
+
+        console.log(result.data.data.users[0].name);
+      } catch (e) {
+        if (axios.isCancel(e)) {
+          console.log("get request cancelled");
+        } else {
+          console.log(e);
+        }
+      }
+    };
+
+    getData();
+
+    return () => {
+      source.cancel("get request cancelled");
+    };
+  }, []);
+  return <div>Hello world!</div>;
 }
 
 export default App;
